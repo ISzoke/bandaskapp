a336c29057c1c6dc0bd1eded38449568281f3e96
Igor Szoke <igor@szoke.cz>
2025-08-15 22:14:47 +0200

    history removed as it causes server overload. Need to be refactored

--- bandaskapp/core/urls.py
+++ bandaskapp/core/urls.py
@@ -5,12 +5,9 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
-    path('history/', views.history, name='history'),
     path('logs/', views.logs, name='logs'),
     path('settings/', views.settings, name='settings'),
     path('api/status/', views.api_status, name='api_status'),
-    path('api/temperature-history/', views.api_temperature_history, name='api_temperature_history'),
-    path('api/temperature-history-hourly/', views.api_temperature_history_hourly, name='api_temperature_history_hourly'),
     path('control/', views.ControlView.as_view(), name='control'),
 ]
 
